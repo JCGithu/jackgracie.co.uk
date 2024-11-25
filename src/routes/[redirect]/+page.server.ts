5ae9071300ed1215b950627aa806732b7e6d916d
JCGithu <jack.c.gracie@gmail.com>
2024-11-25 19:41:33 +0000

    patching the redirect

--- src/routes/[redirect]/+page.server.ts
+++ src/routes/[redirect]/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, EntryGenerator } from './$types';
 const links: Record<string, string> = {
 	donate: 'https://ko-fi.com/colloquial',
 	//wishlist: 'https://www.notion.so/Wishlist-a262268888bf41bab78cb25c1a338639',
@@ -8,6 +8,14 @@ const links: Record<string, string> = {
 	github: "https://github.com/JCGithu",
 };
 
+export const entries: EntryGenerator = () => {
+	return Object.keys(links).map((key) => ({
+		redirect: key
+	}));
+};
+
 export const load: PageServerLoad = async ({ params }) => {
 	return { url: links[params.redirect] };
 };
+
+export const prerender = true;
