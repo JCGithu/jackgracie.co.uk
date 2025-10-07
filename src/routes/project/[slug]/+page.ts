06ef1b2d8b4bfd2147fb8c7ae5d7cc3a5f280d8d
JCGithu <jack.c.gracie@gmail.com>
2025-10-07 19:02:51 +0100

    fixing prerender

--- src/routes/project/[slug]/+page.ts
+++ src/routes/project/[slug]/+page.ts
@@ -1,5 +1,10 @@
-import { loadProject } from '$lib/utils/projects.js';
+import { loadProject, loadAllProjects } from '$lib/utils/projects.js';
 export const prerender = true;
+
+export async function entries() {
+  const projects = await loadAllProjects();
+  return projects.map(project => ({ slug: project.slug }));
+}
 
 export async function load({ params }) {
   let projectSlug: string = params.slug.split('/').pop() || '';
