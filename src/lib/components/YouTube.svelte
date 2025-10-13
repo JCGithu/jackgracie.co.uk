1571d0e9fb74025ed449193234bd8e5de2d8593c
JCGithu <jack.c.gracie@gmail.com>
2025-10-13 18:28:44 +0100

    Youtube embed size forcing

--- src/lib/components/YouTube.svelte
+++ src/lib/components/YouTube.svelte
@@ -20,22 +20,30 @@
   }
 </script>
 
-{#if isLoaded}
-  <iframe src="https://www.youtube.com/embed/{videoId}?autoplay=1" {title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-{:else}
-  <div class="youtube-thumbnail" role="button" tabindex="0" onclick={loadVideo} onkeydown={(e) => e.key === "Enter" && loadVideo()} aria-label="Play {title}">
-    <img src={thumbnailUrl} alt="{title} thumbnail" class="thumbnail-image" loading="lazy" />
+<div class="youtube-container">
+  {#if isLoaded}
+    <iframe src="https://www.youtube.com/embed/{videoId}?autoplay=1" {title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  {:else}
+    <div class="youtube-thumbnail" role="button" tabindex="0" onclick={loadVideo} onkeydown={(e) => e.key === "Enter" && loadVideo()} aria-label="Play {title}">
+      <img src={thumbnailUrl} alt="{title} thumbnail" class="thumbnail-image" loading="lazy" />
 
-    <div class="play-button">
-      <svg width="68" height="48" viewBox="0 0 68 48">
-        <path d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" fill="#f00"></path>
-        <path d="M 45,24 27,14 27,34" fill="#fff"></path>
-      </svg>
+      <div class="play-button">
+        <svg width="68" height="48" viewBox="0 0 68 48">
+          <path d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" fill="#f00"></path>
+          <path d="M 45,24 27,14 27,34" fill="#fff"></path>
+        </svg>
+      </div>
     </div>
-  </div>
-{/if}
+  {/if}
+</div>
 
 <style lang="scss">
+  .youtube-container {
+    aspect-ratio: 16/9;
+    width: 100%;
+    height: 100%;
+  }
+
   iframe {
     width: 100%;
     height: 100%;
@@ -44,6 +52,7 @@
   }
 
   .youtube-thumbnail {
+    aspect-ratio: 16/9;
     width: 100%;
     height: 100%;
     position: relative;
