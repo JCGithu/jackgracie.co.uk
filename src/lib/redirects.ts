871bab270dea98d5ec1ee6e5540dbea5d4894976
JCGithu <jack.c.gracie@gmail.com>
2025-10-19 23:12:17 +0100

    Adding videography redirect

--- src/lib/redirects.ts
+++ src/lib/redirects.ts
@@ -1,6 +1,7 @@
 export default {
   "/mograph": "/motion",
   "/editingreel": "https://youtu.be/GM1dctHM1EM",
+  "/videography": "https://vimeo.com/1123610282?share=copy",
   "/frontier": "https://youtu.be/GM1dctHM1EM",
   "/capturereel": "/capture",
   "/mographreel": "https://www.youtube.com/watch?v=ak7QmAedl2k",
